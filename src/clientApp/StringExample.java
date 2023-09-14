45d007c6c564fc81b9ee974d8f6ba230e1f3d056
Ganesh_Gowda <68227438+GaneshS1504@users.noreply.github.com>
2023-09-14 22:35:10 +0530

    Update StringExample.java

--- src/clientApp/StringExample.java
+++ src/clientApp/StringExample.java
@@ -1,16 +1,10 @@
 package clientApp;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Function;
-import java.util.stream.Collectors;
-
 public class StringExample {
 	
 	
 	public static void main(String[] args) {
-		
+		System.out.println("Hi")
 	}
 
 }
